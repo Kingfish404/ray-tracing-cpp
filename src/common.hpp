484419a17f12b006dee1e00514c052927621891c
Kingfish404 <jinyu929@qq.com>
2021-12-26 01:05:31 +0800

    feat: Antialiasing

--- src/common.hpp
+++ src/common.hpp
@@ -1,7 +1,7 @@
 /**
  * @file common.hpp
  * @author Jin Yu (jin-yu@acm.org)
- * @brief Named rtweekend.h in source of raytracing class
+ * @brief Named `rtweekend.h` in source of raytracing class
  * 
  */
 
@@ -12,6 +12,7 @@
 #include <cmath>
 #include <limits>
 #include <memory>
+#include <cstdlib>
 
 // Using
 
@@ -28,6 +29,31 @@ const double pi = 3.1415926535897932385;
 inline double degrees_to_radians(double degrees)
 {
     return degrees * pi / 180.0;
+}
+
+// @brief Returns a random real in [0,1).
+inline double random_double()
+{
+    return rand() / (RAND_MAX + 1.0);
+}
+
+// @brief Returns a random real in [min,max]
+inline double random_double(double min, double max)
+{
+    return min + (max - min) * random_double();
+}
+
+inline double clamp(double x, double min, double max)
+{
+    if (x < min)
+    {
+        return min;
+    }
+    if (x > max)
+    {
+        return max;
+    }
+    return x;
 }
 
 // Common Headers
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include "common.hpp"
+#include "camera.hpp"
 #include "utils/color.hpp"
 #include "utils/hittable_list.hpp"
 #include "utils/sphere.hpp"
@@ -23,6 +24,7 @@ int main()
     const auto aspect_ratio = 16.0 / 9.0;
     const int image_width = 720;
     const int image_height = static_cast<int>(image_width / aspect_ratio);
+    const int samples_per_pixel = 10;
 
     // World
     hittable_list world;
@@ -30,14 +32,7 @@ int main()
     world.add(make_shared<sphere>(point3(0, -100.5, -1), 100));
 
     // Camera
-    auto viewport_height = 2.0;
-    auto viewport_width = aspect_ratio * viewport_height;
-    auto focal_length = 1.0;
-
-    auto origin = point3(0, 0, 0);
-    auto horizontal = vec3(viewport_width, 0, 0);
-    auto vertical = vec3(0, viewport_height, 0);
-    auto lower_left_corner = origin - horizontal / 2 - vertical / 2 - vec3(0, 0, focal_length);
+    camera cam;
 
     // Render
     std::cout << "P3\n"
@@ -48,13 +43,15 @@ int main()
         // std::cerr << "\rScanlines remaining: " << j << " " << std::flush;
         for (int i = 0; i < image_width; ++i)
         {
-            auto u = double(i) / (image_width - 1);
-            auto v = double(j) / (image_height - 1);
-
-            ray r(origin, lower_left_corner + u * horizontal + v * vertical - origin);
-            color pixel_color = ray_color(r, world);
-
-            write_color(std::cout, pixel_color);
+            color pixel_color(0, 0, 0);
+            for (int s = 0; s < samples_per_pixel; ++s)
+            {
+                auto u = (i + random_double()) / (image_width - 1);
+                auto v = (j + random_double()) / (image_height - 1);
+                ray r = cam.get_ray(u, v);
+                pixel_color += ray_color(r, world);
+            }
+            write_color(std::cout, pixel_color, samples_per_pixel);
         }
     }
     std::cerr << "\nDone.\n";
